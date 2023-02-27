83ce31c05c6581dca843390a3294a4ef060a7185
Md sany <110120520+mdsany11@users.noreply.github.com>
2023-02-27 09:23:51 +0600

    loding animation s

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,6 +124,17 @@
       }
     }
   });
+  
+  
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
 
   /**
    * Hero type effect
